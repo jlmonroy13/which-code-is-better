d0376c2c3b2ff8acf7ec20cae17e8c3decdfd5e2
Selvio Perez <selvio89@hotmail.com>
2024-06-22 18:41:05 -0500

    Set timer with 12am utc time

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { UserInterface } from "@/types/user";
+import { getNextMidnightUT } from "@/utils/date";
 import Image from "next/image";
 import Link from "next/link";
 import Countdown from "react-countdown";
@@ -29,7 +30,7 @@ const Header = ({ user }: HeaderProps) => {
         <div className="flex items-center gap-2">
           Voting ends in:
           <div className="rounded bg-primary px-2 py-1 text-base-100 font-medium min-w-[115px]">
-            <Countdown date={Date.now() + 10000000} />
+            <Countdown date={getNextMidnightUT()} />
           </div>
         </div>
         <div className="flex items-center gap-4">
--- utils/date.ts
+++ utils/date.ts
@@ -5,3 +5,12 @@ export const getCurrentDate = () => {
   const day = String(today.getDate()).padStart(2, "0");
   return "2024-06-17" || `${year}-${month}-${day}`; // Change this when push to prod
 };
+
+export const getNextMidnightUT = () => {
+  const now = new Date();
+  const nextMidnightUTC = new Date(
+    Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 1)
+  );
+  nextMidnightUTC.setUTCHours(0, 0, 0, 0);
+  return nextMidnightUTC;
+};
