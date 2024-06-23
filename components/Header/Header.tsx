f3c8e925e9b5922627c88d6b1cb0cdbb90ef0b48
Jorge Monroy <jlmonroy13@gmail.com>
2024-06-23 11:13:39 -0500

    Fix countdown server/client issue

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,12 +7,20 @@ import Link from "next/link";
 import Countdown from "react-countdown";
 import logo from "../../public/logo.svg";
 import UserProfile from "../UserProfille/UserProfile";
+import { useEffect, useState } from "react";
 
 interface HeaderProps {
   user?: UserInterface | null;
 }
 
 const Header = ({ user }: HeaderProps) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+
   return (
     <header className="z-10 flex flex-col items-start justify-between gap-4 bg-black/20 px-7 py-4 sm:flex-row sm:items-center">
       <nav className="flex items-center gap-14">
@@ -27,12 +35,12 @@ const Header = ({ user }: HeaderProps) => {
         </Link>
       </nav>
       <div className="flex gap-14 items-center">
-        <div className="flex items-center gap-2">
-          Voting ends in:
-          <div className="rounded bg-primary px-2 py-1 text-base-100 font-medium min-w-[115px]">
-            <Countdown date={getNextMidnightUT()} />
+          <div className="flex items-center gap-2">
+            Voting ends in:
+            <div className="rounded bg-primary px-2 py-1 text-base-100 font-medium min-w-[115px] min-h-[32px]">
+              {isClient ? <Countdown date={getNextMidnightUT()} /> : "00:00:00:00"}
+            </div>
           </div>
-        </div>
         <div className="flex items-center gap-4">
           {user?.name || user?.email}
           <UserProfile user={user} />
