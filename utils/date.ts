1ef88bcc10322a6588af447bdc4449b5b2e80867
Selvio Perez <selvio89@hotmail.com>
2024-06-23 11:07:37 -0500

    Update getCurrentDate to return UTC time

--- utils/date.ts
+++ utils/date.ts
@@ -1,8 +1,8 @@
 export const getCurrentDate = () => {
   const today = new Date();
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, "0");
-  const day = String(today.getDate()).padStart(2, "0");
+  const year = today.getUTCFullYear();
+  const month = String(today.getUTCMonth() + 1).padStart(2, "0");
+  const day = String(today.getUTCDate()).padStart(2, "0");
   return "2024-06-17" || `${year}-${month}-${day}`; // Change this when push to prod
 };
 
