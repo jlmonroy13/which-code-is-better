b5cf0c44caf7529917f1b2654a49f3aeb04db105
Selvio Perez <selvio89@hotmail.com>
2024-08-13 08:03:29 -0500

    Minor change

--- utils/date.ts
+++ utils/date.ts
@@ -1,33 +1,20 @@
-const getNowUTC = () => {
-  const now = new Date();
-  return new Date(
-    Date.UTC(
-      now.getUTCFullYear(),
-      now.getUTCMonth(),
-      now.getUTCDate(),
-      now.getUTCHours(),
-      now.getUTCMinutes(),
-      now.getUTCSeconds()
-    )
-  );
-};
-
 export const getCurrentWeek = () => {
-  const now = getNowUTC();
-  const startOfYear = new Date(now.getFullYear(), 0, 1);
-  const weekNumber = Math.ceil(
-    ((now.getTime() - startOfYear.getTime()) / 86400000 +
-      startOfYear.getDay() +
-      1) /
-      7
+  const now = new Date();
+  const startOfYearUTC = new Date(Date.UTC(now.getUTCFullYear(), 0, 1));
+  const nowUTC = new Date(
+    Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate())
   );
-  return `${now.getFullYear()}-W${weekNumber.toString().padStart(2, "0")}`;
+  const timeDifference = nowUTC.getTime() - startOfYearUTC.getTime();
+  const dayOfYearUTC = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) + 1;
+  const weekNumber = Math.ceil((dayOfYearUTC + 1) / 7);
+
+  return `${now.getUTCFullYear()}-W${weekNumber.toString().padStart(2, "0")}`;
 };
 
 export const isWeekPassed = (weekStr: string) => {
   const [year, week] = weekStr.split("-W").map(Number);
-  const currentDate = getNowUTC();
-  const currentYear = currentDate.getFullYear();
+  const currentDate = new Date();
+  const currentYear = currentDate.getUTCFullYear();
   const currentWeek = getCurrentWeek().split("-W")[1];
 
   if (year < currentYear) return true;
