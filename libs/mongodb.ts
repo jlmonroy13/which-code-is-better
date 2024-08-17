e9bb5fb6ff1d45a1413fe15ef0cde121b751231f
Jorge Monroy <jlmonroy13@gmail.com>
2024-08-17 17:02:25 -0500

    Optimize DB connections

--- libs/mongodb.ts
+++ libs/mongodb.ts
@@ -1,16 +1,48 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-export const connectMongoDB = async () => {
-  if (MONGODB_URI) {
-    try {
-      await mongoose.connect(MONGODB_URI);
-      console.log("MongoDB connected");
-    } catch (error) {
-      console.error("MongoDB connection error", error);
-    }
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+}
+
+// Add this type definition
+type CachedMongoose = {
+  conn: mongoose.Connection | null;
+  promise: Promise<mongoose.Connection> | null;
+};
+
+declare global {
+  var mongoose: CachedMongoose | undefined;
+}
+
+let cached = global.mongoose || { conn: null, promise: null };
+
+async function connectMongoDB() {
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise && MONGODB_URI) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log("MongoDB connected");
+      return mongoose.connection;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    console.error("MongoDB connection error", e);
+    throw e;
+  }
+
+  return cached.conn;
 };
 
 export default connectMongoDB;
