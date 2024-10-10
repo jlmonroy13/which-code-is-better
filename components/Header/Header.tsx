1de298f8c6ac7c305433d83cecab741a192d4020
Jorge Monroy <jlmonroy13@gmail.com>
2024-10-10 18:14:07 -0500

    Add prisma generate before build

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,7 +9,7 @@ import { UserInterface } from "@/types/user";
 import { getNextWeekStartUTC } from "@/utils/date";
 
 import logo from "../../public/logo.svg";
-// import CreateRumbleButton from "../CreateRumbleButton";
+import CreateRumbleButton from "../CreateRumbleButton";
 import UserProfile from "../UserProfile";
 
 interface HeaderProps {
@@ -83,7 +83,7 @@ const Header = ({ user }: HeaderProps) => {
           <UserProfile user={user} />
         </div>
       </div>
-      {/* <CreateRumbleButton /> */}
+      <CreateRumbleButton />
     </header>
   );
 };
--- next.config.js
+++ next.config.js
@@ -16,6 +16,29 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Be more specific in production
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
   },
